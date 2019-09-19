90e8815939ebb830f0845e5a74843f7c885fc342
Yurec <yurec133@gmail.com>
2019-09-19 10:16:53 +0300

    Updates

---
+++ precache-manifest.de55896c0e118753b2e062d997ca58f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a36e0a96ba867dd3ff9279cff927e6af",
+    "url": "/squares/index.html"
+  },
+  {
+    "revision": "f318870fc699e0023dfb",
+    "url": "/squares/static/css/main.11b6002c.chunk.css"
+  },
+  {
+    "revision": "9dabb5a77fee3098325b",
+    "url": "/squares/static/js/2.920f917d.chunk.js"
+  },
+  {
+    "revision": "f318870fc699e0023dfb",
+    "url": "/squares/static/js/main.e3ba5bcc.chunk.js"
+  },
+  {
+    "revision": "7357902b0033b03572c9",
+    "url": "/squares/static/js/runtime~main.1c4b5a4d.js"
+  }
+]);
